12c87bb445540c60017a9511b256f0145ba2507a
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-02-25 14:54:17 +0500

    Сделал нормально LimitedSizeStack.cs

--- LimitedSizeStack/LimitedSizeStack.cs
+++ LimitedSizeStack/LimitedSizeStack.cs
@@ -1,94 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LimitedSizeStack;
 
 public class LimitedSizeStack<T>
 {
-    private Element<T> Head { get; set; }
-    private Element<T> Tail { get; set; }
-    private int Limit { get; set; }
-    public int Count { get; private set; }
+    private int size;
+    private LinkedList<T> items = new();
 
-    public LimitedSizeStack(int undoLimit)
+    public int Count
     {
-        Limit = undoLimit;
+        get
+        {
+            if (items.Count != null)
+                return items.Count;
+            else
+                return 0; //Если лист (стек) пустой
+        }
     }
+
+    public LimitedSizeStack(int limit) => size = limit;
 
     public void Push(T item)
     {
-        if (IsListEmpty())
-        {
-            if (Limit > 0)
-            {
-                AddFirstElement(item);
-            }
-        }
-        else
-        {
-            if (Limit == Count)
-            {
-                RemoveFirstElement();
-            }
-
-            AddToTail(item);
-        }
-    }
-
-    private bool IsListEmpty()
-    {
-        return Head == null && Tail == null;
-    }
-
-    private void AddFirstElement(T item)
-    {
-        Head = Tail = new Element<T> { Value = item };
-        Count++;
-    }
-
-    private void RemoveFirstElement()
-    {
-        Head = Head.Next;
-        if (Head != null)
-        {
-            Head.Prev = null;
-        }
-        Count--;
-    }
-
-    private void AddToTail(T item)
-    {
-        var newItem = new Element<T> { Value = item, Prev = Tail };
-        Tail.Next = newItem;
-        Tail = newItem;
-        Count++;
+        if (size == 0) //Если не задан размер то выходим
+            return;
+        if (items.Count >= size)
+            items.RemoveFirst();
+        items.AddLast(item);
     }
 
     public T Pop()
     {
-        if (Count == 0 || Limit == 0)
-        {
-            throw new Exception();
-        }
-
-        var result = Tail.Value;
-        if (Count == 1)
-        {
-            Head = null;
-            Tail = null;
-            Count--;
-        }
-        else
-        {
-            Tail = Tail.Prev;
-            Count--;
-        }
-
-        return result;
+        if (items.Count == 0) // И вот если лист пустой то выводим Exception 
+            throw new InvalidOperationException("Стек пустой");
+        var lastValue = items.Last.Value;
+        items.RemoveLast();
+        return lastValue;
     }
-}
-public class Element<T>
-{
-    public T Value;
-    public Element<T> Next;
-    public Element<T> Prev;
 }
