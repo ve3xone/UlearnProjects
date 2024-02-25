19626a40f744c226600e30cf9832e4e2f7a70893
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-02-25 16:14:03 +0500

    Рабочая практика «Отмена»

--- LimitedSizeStack/ListModel.cs
+++ LimitedSizeStack/ListModel.cs
@@ -3,38 +3,75 @@ using System.Collections.Generic;
 
 namespace LimitedSizeStack;
 
+public class HistoryItem<TItem>
+{
+    public TItem Value;
+    public Actions Action;
+    public int Index;
+}
+
+public enum Actions
+{
+    Add,
+    Remove
+}
+
 public class ListModel<TItem>
 {
-	public List<TItem> Items { get; }
-	public int UndoLimit;
-        
-	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-	{
-	}
+    private readonly LimitedSizeStack<HistoryItem<TItem>>   History;
+    public readonly List<TItem> Items;
+    public int UndoLimit;
 
-	public ListModel(List<TItem> items, int undoLimit)
-	{
-		Items = items;
-		UndoLimit = undoLimit;
-	}
+    public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit){ }
 
-	public void AddItem(TItem item)
-	{
-		Items.Add(item);
-	}
+    public ListModel(List<TItem> items, int undoLimit)
+    {
+        Items = items;
+        UndoLimit = undoLimit;
+        History = new LimitedSizeStack<HistoryItem<TItem>>(undoLimit);
+    }
 
-	public void RemoveItem(int index)
-	{
-		Items.RemoveAt(index);
-	}
+    public void AddItem(TItem item)
+    {
+        Items.Add(item);
+        History.Push(new HistoryItem<TItem> { Value = item, Action = Actions.Add });
+    }
 
-	public bool CanUndo()
-	{
-		return false;
-	}
+    public void RemoveItem(int index)
+    {
+        if (index < 0 || index >= Items.Count)
+            throw new ArgumentOutOfRangeException(nameof(index));
 
-	public void Undo()
-	{
-		throw new NotImplementedException();
-	}
+        var removedItem = Items[index];
+        Items.RemoveAt(index);
+        History.Push(new HistoryItem<TItem> { Value = removedItem, Action = Actions.Remove, Index = index });
+    }
+
+    public bool CanUndo()
+    {
+        return History.Count > 0;
+    }
+
+    public void Undo()
+    {
+        if (CanUndo())
+        {
+            var lastAction = History.Pop();
+            if (lastAction.Action == Actions.Add)
+            {
+                Items.Remove(lastAction.Value);
+            }
+            else if (lastAction.Action == Actions.Remove)
+            {
+                if (lastAction.Index >= 0 && lastAction.Index < Items.Count)
+                {
+                    Items.Insert(lastAction.Index, lastAction.Value);
+                }
+                else
+                {
+                    Items.Add(lastAction.Value);
+                }
+            }
+        }
+    }
 }
