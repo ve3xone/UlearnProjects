41f4956b8c9350d83684e46b1945967a86b7acf4
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-02-29 00:09:41 +0500

    Практика «Скользящий максимум» (пустая пока-что)

---
+++ Smooth/MovingMaxTask.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace yield;
+
+public static class MovingMaxTask
+{
+	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+	{
+		//Fix me!
+		return data;
+	}
+}
