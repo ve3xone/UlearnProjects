9bb8eefb053954a7a58de0e5780c0e1fa944ac98
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-03-07 08:47:09 +0500

    Доделал практику «Скользящий максимум»

--- Smooth/MovingMaxTask.cs
+++ Smooth/MovingMaxTask.cs
@@ -4,9 +4,26 @@ namespace yield;
 
 public static class MovingMaxTask
 {
-	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-	{
-		//Fix me!
-		return data;
-	}
+    public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+    {
+        var windowMaxs = new LinkedList<double>();
+        var window = new Queue<double>();
+
+        foreach (var dataPoint in data)
+        {
+            if (window.Count == windowWidth)
+            {
+                var removed = window.Dequeue();
+                if (windowMaxs.First.Value.Equals(removed)) windowMaxs.RemoveFirst();
+            }
+
+            while (windowMaxs.Count > 0 && windowMaxs.Last.Value < dataPoint.OriginalY)
+                windowMaxs.RemoveLast();
+
+            windowMaxs.AddLast(dataPoint.OriginalY);
+            window.Enqueue(dataPoint.OriginalY);
+
+            yield return dataPoint.WithMaxY(windowMaxs.First.Value);
+        }
+    }
 }
