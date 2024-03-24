8d5a0ebda1c5c0846cd18a018d79c65022d8f57e
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-03-24 14:53:37 +0500

    Поправил практику «Циклы Brainfuck»
    Сделал чтоб использовать несколько раз BrainFuck вывел bracket и stack в инициализацию, вроде как то что просили сделал ну хз может позже ещё поправлю

--- 2-semester/practices/brainfuck/BrainfuckLoopCommands.cs
+++ 2-semester/practices/brainfuck/BrainfuckLoopCommands.cs
@@ -1,39 +1,42 @@
 using System.Collections.Generic;
 
-namespace func.brainfuck;
-
-public class BrainfuckLoopCommands
+namespace func.brainfuck
 {
-    private static Dictionary<int, int> bracket = new ();
-    private static Stack<int> stack = new ();
-
-    public static void RegisterTo(IVirtualMachine vm)
+    public class BrainfuckLoopCommands
     {
-        InitializeBrackets(vm.Instructions);
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var bracket = InitializeBrackets(vm.Instructions);
 
-        vm.RegisterCommand('[', b =>
-        {
-            if (b.Memory[b.MemoryPointer] == 0)
-                b.InstructionPointer = bracket[b.InstructionPointer];
-        });
-        vm.RegisterCommand(']', b =>
-        {
-            if (b.Memory[b.MemoryPointer] != 0)
-                b.InstructionPointer = bracket[b.InstructionPointer];
-        });
-    }
-
-    private static void InitializeBrackets(string instructions)
-    {
-        for (var i = 0; i < instructions.Length; i++)
-        {
-            if (instructions[i] == '[') 
-                stack.Push(i);
-            else if (instructions[i] == ']')
+            vm.RegisterCommand('[', b =>
             {
-                bracket[i] = stack.Peek();
-                bracket[stack.Pop()] = i;
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = bracket[b.InstructionPointer];
+            });
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                    b.InstructionPointer = bracket[b.InstructionPointer];
+            });
+        }
+
+        private static Dictionary<int, int> InitializeBrackets(string instructions)
+        {
+            var bracket = new Dictionary<int, int>();
+            var stack = new Stack<int>();
+
+            for (var i = 0; i < instructions.Length; i++)
+            {
+                if (instructions[i] == '[')
+                    stack.Push(i);
+                else if (instructions[i] == ']')
+                {
+                    bracket[i] = stack.Peek();
+                    bracket[stack.Pop()] = i;
+                }
             }
+
+            return bracket;
         }
     }
 }
