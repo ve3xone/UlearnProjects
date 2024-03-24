c39eb9db776f49a7356c056929916169d3f9f24d
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-03-24 15:16:44 +0500

    Поправил практика «Простые команды Brainfuck»
    Убрал не нужный unchecked

--- 2-semester/practices/brainfuck/BrainfuckBasicCommands.cs
+++ 2-semester/practices/brainfuck/BrainfuckBasicCommands.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 namespace func.brainfuck;
 
@@ -7,44 +8,26 @@ public static class BrainfuckBasicCommands
     public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
     {
         vm.RegisterCommand('.', (machine) => write((char)machine.Memory[machine.MemoryPointer]));
-        vm.RegisterCommand(',', (machine) =>
-        {
-            int input = read();
-            if (input != -1)
-            {
-                machine.Memory[machine.MemoryPointer] = (byte)input;
-            }
-        });
-        vm.RegisterCommand('+', (machine) => AddToValueInMemory(machine, +1));
-        vm.RegisterCommand('-', (machine) => AddToValueInMemory(machine, -1));
-        vm.RegisterCommand('>', (machine) => AddToMemoryPointer(machine, +1));
-        vm.RegisterCommand('<', (machine) => AddToMemoryPointer(machine, -1));
+        vm.RegisterCommand(',', (machine) => { machine.Memory[machine.MemoryPointer] = (byte)read(); });
+
+        vm.RegisterCommand('+', (machine) => machine.Memory[machine.MemoryPointer] = (byte)ModuleSum(machine.Memory[machine.MemoryPointer], 1, 256));
+        vm.RegisterCommand('-', (machine) => machine.Memory[machine.MemoryPointer] = (byte)ModuleSum(machine.Memory[machine.MemoryPointer], -1, 256));
+        vm.RegisterCommand('>', (machine) => machine.MemoryPointer = ModuleSum(machine.MemoryPointer, 1, machine.Memory.Length));
+        vm.RegisterCommand('<', (machine) => machine.MemoryPointer = ModuleSum(machine.MemoryPointer, -1, machine.Memory.Length));
 
         RegisterCommandForSymbolRange(vm, 'A', 'Z');
         RegisterCommandForSymbolRange(vm, 'a', 'z');
         RegisterCommandForSymbolRange(vm, '0', '9');
     }
 
-    private static void AddToValueInMemory(IVirtualMachine vm, int value)
-    {
-        vm.Memory[vm.MemoryPointer] = unchecked((byte)(vm.Memory[vm.MemoryPointer] + value));
-    }
-
-    private static void AddToMemoryPointer(IVirtualMachine vm, int value)
-    {
-        vm.MemoryPointer += value;
-        if (vm.MemoryPointer == vm.Memory.Length)
-            vm.MemoryPointer = 0;
-        else if (vm.MemoryPointer == -1)
-            vm.MemoryPointer = vm.Memory.Length - 1;
-    }
+    private static int ModuleSum(int current, int shift, int module) => (current + shift + module) % module;
 
     private static void RegisterCommandForSymbolRange(IVirtualMachine vm, char start, char end)
     {
         for (char c = start; c <= end; c++)
         {
             char tmp = c;
-            vm.RegisterCommand(c, (machine) => machine.Memory[machine.MemoryPointer] = (byte)tmp);
+            vm.RegisterCommand(tmp, (machine) => machine.Memory[machine.MemoryPointer] = (byte)tmp);
         }
     }
 }
