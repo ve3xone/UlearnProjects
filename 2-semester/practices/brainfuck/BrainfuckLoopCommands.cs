ee97e18978c39ec6ff2f1bc7a4dad2c8171f3355
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-03-24 16:06:59 +0500

    Поправил практику «Циклы Brainfuck» 2
    убрал у namespace скобки так как net6 же и выглядит красивее так

--- 2-semester/practices/brainfuck/BrainfuckLoopCommands.cs
+++ 2-semester/practices/brainfuck/BrainfuckLoopCommands.cs
@@ -1,42 +1,41 @@
 using System.Collections.Generic;
 
-namespace func.brainfuck
+namespace func.brainfuck;
+
+public class BrainfuckLoopCommands
 {
-    public class BrainfuckLoopCommands
+    public static void RegisterTo(IVirtualMachine vm)
     {
-        public static void RegisterTo(IVirtualMachine vm)
+        var bracket = InitializeBrackets(vm.Instructions);
+
+        vm.RegisterCommand('[', b =>
         {
-            var bracket = InitializeBrackets(vm.Instructions);
-
-            vm.RegisterCommand('[', b =>
-            {
-                if (b.Memory[b.MemoryPointer] == 0)
-                    b.InstructionPointer = bracket[b.InstructionPointer];
-            });
-            vm.RegisterCommand(']', b =>
-            {
-                if (b.Memory[b.MemoryPointer] != 0)
-                    b.InstructionPointer = bracket[b.InstructionPointer];
-            });
-        }
-
-        private static Dictionary<int, int> InitializeBrackets(string instructions)
+            if (b.Memory[b.MemoryPointer] == 0)
+                b.InstructionPointer = bracket[b.InstructionPointer];
+        });
+        vm.RegisterCommand(']', b =>
         {
-            var bracket = new Dictionary<int, int>();
-            var stack = new Stack<int>();
+            if (b.Memory[b.MemoryPointer] != 0)
+                b.InstructionPointer = bracket[b.InstructionPointer];
+        });
+    }
 
-            for (var i = 0; i < instructions.Length; i++)
+    private static Dictionary<int, int> InitializeBrackets(string instructions)
+    {
+        var bracket = new Dictionary<int, int>();
+        var stack = new Stack<int>();
+
+        for (var i = 0; i < instructions.Length; i++)
+        {
+            if (instructions[i] == '[')
+                stack.Push(i);
+            else if (instructions[i] == ']')
             {
-                if (instructions[i] == '[')
-                    stack.Push(i);
-                else if (instructions[i] == ']')
-                {
-                    bracket[i] = stack.Peek();
-                    bracket[stack.Pop()] = i;
-                }
+                bracket[i] = stack.Peek();
+                bracket[stack.Pop()] = i;
             }
-
-            return bracket;
         }
+
+        return bracket;
     }
 }
