f6afbaa2c1efc2b89cc1ad4caaa522db42870a41
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-03-29 01:13:13 +0500

    Поправил практику «Median & Bigrams»
    Поправил табуляцию

--- 2-semester/practices/linq-slideviews/ExtensionsTask.cs
+++ 2-semester/practices/linq-slideviews/ExtensionsTask.cs
@@ -6,19 +6,19 @@ namespace linq_slideviews;
 
 public static class ExtensionsTask
 {
-	/// <summary>
-	/// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
-	/// Медиана списка из четного количества элементов — это среднее арифметическое 
+    /// <summary>
+    /// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
+    /// Медиана списка из четного количества элементов — это среднее арифметическое
     /// двух серединных элементов списка после сортировки.
-	/// </summary>
-	/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
-	public static double Median(this IEnumerable<double> items)
-	{
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
+    public static double Median(this IEnumerable<double> items)
+    {
         if (items == null)
             throw new InvalidOperationException();
 
-		var sortedItems = items.OrderBy(x => x).ToList();
-		var middleIndex = sortedItems.Count / 2;
+        var sortedItems = items.OrderBy(x => x).ToList();
+        var middleIndex = sortedItems.Count / 2;
 
         if (sortedItems.Count == 0)
             throw new InvalidOperationException();
@@ -28,12 +28,12 @@ public static class ExtensionsTask
             return (sortedItems[middleIndex - 1] + sortedItems[middleIndex]) / 2;
     }
 
-	/// <returns>
-	/// Возвращает последовательность, состоящую из пар соседних элементов.
-	/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
-	/// </returns>
-	public static IEnumerable<(T First, T Second)> Bigrams<T>(this IEnumerable<T> items)
-	{
+    /// <returns>
+    /// Возвращает последовательность, состоящую из пар соседних элементов.
+    /// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
+    /// </returns>
+    public static IEnumerable<(T First, T Second)> Bigrams<T>(this IEnumerable<T> items)
+    {
         using var enumerator = items.GetEnumerator();
 
         if (!enumerator.MoveNext())
