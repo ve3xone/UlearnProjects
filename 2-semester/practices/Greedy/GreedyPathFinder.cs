ff3eaaadc74acd31016352bd51c8d93f7bc48046
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-04-26 11:11:46 +0500

    Практика «Жадина в лабиринте»

--- 2-semester/practices/Greedy/GreedyPathFinder.cs
+++ 2-semester/practices/Greedy/GreedyPathFinder.cs
@@ -1,12 +1,36 @@
-﻿using System.Collections.Generic;
-using Greedy.Architecture;
+﻿using Greedy.Architecture;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Greedy;
-
-public class GreedyPathFinder : IPathFinder
+namespace Greedy
 {
-	public List<Point> FindPathToCompleteGoal(State state)
-	{
-		return new List<Point>();
-	}
+    public class GreedyPathFinder : IPathFinder
+    {
+        public List<Point> FindPathToCompleteGoal(State state)
+        {
+            var pathToGoal = new List<Point>();
+            var chests = state.Chests.ToList();
+            if (chests.Count < state.Goal)
+                return pathToGoal;
+
+            var pathFinder = new DijkstraPathFinder();
+            var stamina = state.InitialEnergy;
+            while (state.Scores < state.Goal)
+            {
+                var pathsToChests = pathFinder.GetPathsByDijkstra(state, state.Position, chests);
+                var pathToBestChest = pathsToChests.FirstOrDefault(path => path.Path.Any());
+                if (pathToBestChest == null)
+                    return pathToGoal;
+                stamina -= pathToBestChest.Cost;
+                if (stamina < 0)
+                    return pathToGoal;
+                pathToGoal.AddRange(pathToBestChest.Path.Skip(1));
+                state.Position = pathToBestChest.Path.Last();
+                chests.Remove(state.Position);
+                state.Scores++;
+            }
+
+            return pathToGoal;
+        }
+    }
 }
