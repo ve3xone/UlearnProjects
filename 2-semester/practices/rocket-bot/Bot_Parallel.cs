b5663b89284b4d971f063960cbd036bdc7cf9b95
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-10 01:34:02 +0500

    Практика «Параллельный AI»

--- 2-semester/practices/rocket-bot/Bot_Parallel.cs
+++ 2-semester/practices/rocket-bot/Bot_Parallel.cs
@@ -1,25 +1,40 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace rocket_bot;
 
 public partial class Bot
 {
-	public Rocket GetNextMove(Rocket rocket)
-	{
-		// TODO: распараллелить запуск SearchBestMove
-		var (turn, score) = CreateTasks(rocket)
-			.First()
-			.GetAwaiter()
-			.GetResult();
-		
-		return rocket.Move(turn, level);
-	}
-	
-	public List<Task<(Turn Turn, double Score)>> CreateTasks(Rocket rocket)
-	{
-		return new() { Task.Run(() => SearchBestMove(rocket, new Random(random.Next()), iterationsCount)) };
-	}
+    public Rocket GetNextMove(Rocket rocket)
+    {
+        var tasks = CreateTasks(rocket);
+        var completedTask = Task.WhenAny(tasks).Result;
+
+        if (completedTask != null)
+        {
+            return rocket.Move(completedTask.Result.Turn, level);
+        }
+        else
+        {
+            throw new InvalidOperationException("No completed tasks found.");
+        }
+    }
+
+    public List<Task<(Turn Turn, double Score)>> CreateTasks(Rocket rocket)
+    {
+        var tasks = new List<Task<(Turn Turn, double Score)>>();
+        var iterationsCountPerThread = iterationsCount / threadsCount;
+
+        Parallel.For(0, threadsCount, i =>
+        {
+            var randomInstance = new Random(random.Next());
+            tasks.Add(Task.Run(() =>
+            {
+                return SearchBestMove(rocket, randomInstance, iterationsCountPerThread);
+            }));
+        });
+
+        return tasks;
+    }
 }
