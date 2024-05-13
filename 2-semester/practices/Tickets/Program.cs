5de8233df18628d61427d2a450e9fe8a67481092
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-14 00:24:22 +0500

    Инициализация проекта Tickets

---
+++ 2-semester/practices/Tickets/Program.cs
@@ -0,0 +1,11 @@
+﻿using NUnitLite;
+
+namespace Tickets;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		new AutoRun().Execute(args);
+	}
+}
---
+++ 2-semester/practices/Tickets/TicketsTask_should.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Numerics;
+
+namespace Tickets;
+
+[TestFixture]
+public class TicketsTask_should
+{
+	private static void MakeTest(int halfLen, int totalSum, string answer)
+	{
+		var result = BigInteger.Parse(answer);
+		Assert.AreEqual(result, TicketsTask.Solve(halfLen, totalSum));
+	}
+
+	[Test]
+	public void Test1() { MakeTest(2, 2, "4"); }
+	[Test]
+	public void Test2() { MakeTest(2, 5, "0"); }
+	[Test]
+	public void Test6() { MakeTest(10, 10, "4008004"); }
+	[Test]
+	public void Test7() { MakeTest(20, 20, "401200499400100"); }
+}
